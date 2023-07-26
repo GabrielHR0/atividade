1ce862f6929de53493805807080a2c65b5d94256
GabrielHR0 <ponderous1734@gmail.com>
2023-07-26 17:51:10 -0300

    a

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Livewire\Site;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/site', Site::class);
